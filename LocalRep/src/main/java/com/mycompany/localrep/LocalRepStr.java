1b48e733450d70dcddb83d662f53f5c03e7aad2a
AlexLynx96 <nastyat1996@yandex.ru>
2021-10-16 17:26:05 +0300

    из локального репозитория

---
+++ LocalRep/src/main/java/com/mycompany/localrep/LocalRepStr.java
@@ -0,0 +1,6 @@
+package com.mycompany.localrep;
+
+public class LocalRepStr {
+	public static String str = "Работа с зависимостью из локального репозитория";
+	public static String strlab = "лаб 2 часть 2";
+}
