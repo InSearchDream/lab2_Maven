c8e2ee65ef7a46817d29fa210d78291f85086a9f
AlexLynx96 <nastyat1996@yandex.ru>
2021-10-16 17:26:27 +0300

    работа с локальным репозиторием

---
+++ mavLocRep/src/main/java/com/mycompany/mavlocrep/mavLocRepStr.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavlocrep;
+
+import com.mycompany.localrep.LocalRepStr;
+
+public class mavLocRepStr {
+    public static void main(String[] args) {
+	System.out.println(LocalRepStr.strlab + '\n' + LocalRepStr.str);
+    }
+}
